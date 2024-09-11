39eb4f1f1c827551b5d0a34914f981995bbbc5c5
Rogelio Martinez <rogejr89@gmail.com>
2024-09-11 18:23:58 -0400

    changing list to json object

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const orderStr = prompt(
     "vanilla,vanilla,vanilla,strawberry,coffee,coffee,vanilla,strawberry,coffee,chocolate,peanut butter,peanut butter,chocolate"
 );
 
-
 const orderArr = orderStr.split(/,/);
 const listOfItemsOrdered = getListOfItems(orderArr);
 
 function getListOfItems(order) {
-    const list = [];
+    const list = {};
     for (const itme of order) {
         console.log(itme);
         if (!list.includes(itme)) {
-            console.log(`item: ${itme} is NOT in array.`);
-            list.push(itme);
+            list[itme];
+            //list.push(itme);
         }
     }
     return list;
 }
 
-const flavorListing = getFlavorListing(listOfItemsOrdered, orderArr);
+
+
+//const flavorListing = getFlavorListing(listOfItemsOrdered, orderArr);
 
 
 //console.log(orderArr);
-//console.log(listOfItemsOrdered);
+console.log(listOfItemsOrdered);
