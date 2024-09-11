a2e2d1a7bb39abde2fd7f00be93dcaf07616cee6
Rogelio Martinez <rogejr89@gmail.com>
2024-09-11 16:21:13 -0400

    created function getListOfItems to get list of itmes

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // create prompt
-prompt("");
+const orderStr = prompt(
+    "Enter your froyo order.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee,vanilla,strawberry,coffee,chocolate,peanut butter,peanut butter,chocolate"
+);
+
+
+const orderArr = orderStr.split(/,/);
+const listOfItemsOrdered = getListOfItems(orderArr);
+
+function getListOfItems(order) {
+    const list = [];
+    for (const itme of order) {
+        console.log(itme);
+        if (!list.includes(itme)) {
+            console.log(`item: ${itme} is NOT in array.`);
+            list.push(itme);
+        }
+    }
+    return list;
+}
+
+const flavorListing = getFlavorListing(listOfItemsOrdered, orderArr);
+
+
+//console.log(orderArr);
+//console.log(listOfItemsOrdered);
